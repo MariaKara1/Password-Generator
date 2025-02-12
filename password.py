847ef6c0da8b4606bec3cd7c3ba8f4c8c0af8e16
Maria Karapetyan <mkartist528@gmail.com>
2025-02-12 18:22:57 +0000

    replacing letters

--- password.py
+++ password.py
@@ -27,11 +27,21 @@ def fetch_word():
     word = response.json()[0]
     return word 
 
+def replaceLetters(word):
+    word = word[0].upper() + word[1:]
+    if "a" in word:
+        word = word.replace("a", "@")
+
+    return word
+
+
 #function to generate  weaker but memorable password
 def generate_weaker_password():
     word1 = fetch_word()
     word2 = fetch_word()
     password = word1 + word2
+    word1 = replaceLetters(word1)
+    word2 = replaceLetters(word2)
     return "Your new weaker password is " + password 
 
 print(generate_weaker_password())
